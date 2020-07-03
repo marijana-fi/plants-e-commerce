11d0c097351dedbc38745806ea34e8cca0ef90c4
marijana-fi <61113052+marijana-fi@users.noreply.github.com>
2020-07-03 14:14:52 +0200

    add test path

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,12 @@ const stripe = require("stripe")(process.env.REACT_APP_STRIPE_SECRET_KEY);
 const express = require("express");
 const router = express.Router();
 
+router.get("/test", (req, res) => {
+	res.json({
+		hello: "hi!",
+	});
+});
+
 router.post("/", async (req, res) => {
 	const body = req.body;
 
