3ad936f5ccbd993f28de8bc46f61514fad00e1b9
marijana-fi <61113052+marijana-fi@users.noreply.github.com>
2020-07-03 14:34:12 +0200

    update server

--- server/server.js
+++ server/server.js
@@ -1,19 +1,38 @@
+const env = require("dotenv").config();
+const stripe = require("stripe")(process.env.REACT_APP_STRIPE_SECRET_KEY);
 const express = require("express");
 const path = require("path");
 const serverless = require("serverless-http");
-const apiRouter = require("./routes");
+const router = express.Router();
 const cors = require("cors");
 const app = express();
+
+router.post("/", async (req, res) => {
+	const body = req.body;
+
+	const options = {
+		...body,
+		amount: req.body.amount,
+		currency: "USD",
+	};
+
+	try {
+		const paymentIntent = await stripe.paymentIntents.create(options);
+		res.json(paymentIntent);
+	} catch (err) {
+		res.json(err);
+	}
+});
 
 let p = path.join(__dirname, "../public");
 
 app.use(express.static(p));
 app.use(express.json());
 app.use(cors());
-app.use(`/.netlify/functions/server`, apiRouter);
+app.use(`/.netlify/functions/server`, router);
 
-app.listen(process.env.PORT || 8080, () => {
-	console.log("listening on port 8080");
-});
+// app.listen(process.env.PORT || 8080, () => {
+// 	console.log("listening on port 8080");
+// });
 
 module.exports.handler = serverless(app);
